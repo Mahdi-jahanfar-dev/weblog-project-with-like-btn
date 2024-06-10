a8dd49f1db1860b05cb42a6bd084e53c72556b24
Mr.JF <mahdi.jahanfar1384@gmail.com>
2024-06-10 20:48:39 +0330

    login class deleted

--- account/views.py
+++ account/views.py
@@ -1,8 +1,3 @@
 from django.shortcuts import render
 from django.views import View
 
-
-class LoginView(View):
-
-    def get(self, request):
-        return render(request, 'account/sign_in.html', {})
