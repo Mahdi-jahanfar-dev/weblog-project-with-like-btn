91419d51fb2bbc995e3563c0608d9abfa749177d
Mr.JF <mahdi.jahanfar1384@gmail.com>
2024-06-10 22:44:33 +0330

    blog model added

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,13 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=50)
+    content = models.TextField()
+    date_posted = models.DateTimeField(auto_now_add=True)
+    image = models.ImageField(upload_to='post_media')
+
+    def __str__(self):
+        return self.title
+
