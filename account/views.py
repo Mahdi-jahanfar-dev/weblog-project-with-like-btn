4139b3ebbc27cce1b83d6f78260d013d307b1ed9
Mr.JF <mahdi.jahanfar1384@gmail.com>
2024-06-10 21:27:48 +0330

    login part completed

--- account/views.py
+++ account/views.py
@@ -1,3 +1,6 @@
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
 from django.shortcuts import render, redirect
 from django.urls import reverse
 from django.views import View
@@ -9,3 +12,14 @@ class LoginView(View):
         if request.user.is_authenticated:
             return redirect(reverse('main:index_view'))
         return render(request, self.template_name, {})
+
+    def post(self, request, *args, **kwargs):
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+
+        user = authenticate(username=username, password=password, email=email)
+        if user is not None:
+            login(request, user)
+            return redirect(reverse('main:index_view'))
+        raise ValidationError('Invalid username or password or email')
