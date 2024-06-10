23c515fedde779361877c27f1df8146b786c9f24
Mr.JF <mahdi.jahanfar1384@gmail.com>
2024-06-10 20:56:58 +0330

    login class added

--- account/views.py
+++ account/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse
 from django.views import View
 
+class LoginView(View):
+    template_name = 'account/sign_in.html'
+
+    def get(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return redirect(reverse('main:index_view'))
+        return render(request, self.template_name, {})
