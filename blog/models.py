fcac7abe7c89c7d3282389b73026c44ed0ec3cbe
Mr.JF <mahdi.jahanfar1384@gmail.com>
2024-06-12 21:56:45 +0330

    slug field added and post detail view created

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,7 @@
 from django.contrib.auth.models import User
 from django.db import models
+from django.utils.text import slugify
+
 
 class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -7,7 +9,13 @@ class Post(models.Model):
     content = models.TextField()
     date_posted = models.DateTimeField(auto_now_add=True)
     image = models.ImageField(upload_to='post_media')
+    slug = models.SlugField(null=True, blank=True, unique=True)
 
+    def save(
+        self, force_insert=False, force_update=False, using=None, update_fields=None
+    ):
+        self.slug = slugify(self.title, allow_unicode=True)
+        super(Post, self).save()
     def __str__(self):
         return self.title
 
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views.generic import DetailView
 
-# Create your views here.
+from .models import Post
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'blog/post_detail.html'
+    context_object_name = 'post'
