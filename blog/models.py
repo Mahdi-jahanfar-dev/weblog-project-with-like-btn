967598cabf39b5386e14d148b5f3ef2b4c0a6c7c
Mr.JF <mahdi.jahanfar1384@gmail.com>
2024-06-13 17:54:35 +0330

    comment models created

--- blog/models.py
+++ blog/models.py
@@ -40,3 +40,15 @@ class Post(models.Model):
         return self.title
     def get_absolute_url(self):
         return reverse('blog:blog_detail', kwargs={'slug': self.slug})
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='comments')
+    content = models.TextField()
+    date_posted = models.DateTimeField(auto_now_add=True)
+    parent = models.ForeignKey('self', on_delete=models.CASCADE, related_name='replies', null=True, blank=True)
+
+    def __str__(self):
+        return self.content[:50]
+
