2ccb322f02d27d1f6e69c02e52a8169004b2cbd5
Mr.JF <mahdi.jahanfar1384@gmail.com>
2024-06-13 19:06:29 +0330

    adding comment form with no parent added

--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,17 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.views.generic import DetailView, ListView
 
-from .models import Post, Category
+from .models import Post, Category, Comment
 
 class PostDetailView(DetailView):
     model = Post
     template_name = 'blog/post_detail.html'
     context_object_name = 'post'
+    def post(self, request, *args, **kwargs):
+        content = request.POST.get('content')
+        post = get_object_or_404(Post, slug=self.kwargs['slug'])
+        Comment.objects.create(post=post, content=content, author=self.request.user)
+        return redirect('blog:blog_detail', slug=self.kwargs['slug'])
 
 
 class CategoryDetailView(ListView):
